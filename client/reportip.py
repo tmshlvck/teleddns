d58a3ab917f53795d8e46dbf37028237e54dba7f
Tomas Hlavacek <tmshlvck@gmail.com>
2019-01-24 09:29:03 +0100

    Replace RESTful report protocol with direct DynDNS
    Replace RESTful report protocol with direct update using nsupdate.
    TSIG-signed update transaction is needed.

--- client/reportip.py
+++ client/reportip.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #
 # DDNS
-# (C) 2015-2017 Tomas Hlavacek (tmshlvck@gmail.com)
+# (C) 2015-2019 Tomas Hlavacek (tmshlvck@gmail.com)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,9 +20,6 @@
 import sys
 import getopt
 import os
-import re
-import httplib2
-import json
 import syslog
 import subprocess
 import re
@@ -30,122 +27,195 @@ import ipaddress
 import time
 
 sys.path.insert(0, '/etc/ddns')
-from reportipconf import *
+import reportipconf as config
 
+
+debug = config.debug
 
 def d(message):
-        if debug:
-                print(message)
+    if debug:
+        print(message)
 
 def log(message):
-        if debug:
-                print(message)
-                syslog.syslog(message)
+    if debug:
+        print(message)
+        syslog.syslog(message)
+    else:
+        syslog.syslog(message)
+
+
+
+def get_dev_ipaddr(dev=None):
+    ipv4regexp=re.compile(r'^\s+inet\s+(([0-9]{1,3}\.){3}[0-9]{1,3})/[0-9]{1,2}\s+')
+    ipv6regexp=re.compile(r'^\s+inet6\s+(([0-9a-fA-F]{0,4}:){0,7}[0-9a-fA-F]{0,4})/[0-9]{1,3}\s+')
+
+    if dev:
+        if type(dev) is str:
+            dev_list = [dev]
         else:
-                syslog.syslog(message)
+            dev_list = dev
+    else:
+        dev_list = [None]
+
+    for d in dev_list:
+        if d:
+            c = [config.bin_ip, 'address', 'show', 'dev', d]
+        else:
+            c = [config.bin_ip, 'address', 'show']
+
+        p=subprocess.Popen(c, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        (stdoutdata, stderrdata)=p.communicate()
+
+        if stdoutdata:
+            for l in stdoutdata.decode('ascii').split('\n'):
+                m = ipv4regexp.match(l)
+                if m:
+                    yield(m.group(1), 4)
+
+                m = ipv6regexp.match(l)
+                if m:
+                    yield(m.group(1), 6)
 
 
-ipv4regexp=re.compile(r'^\s+inet\s+(([0-9]{1,3}\.){3}[0-9]{1,3})/[0-9]{1,2}\s+')
-ipv6regexp=re.compile(r'^\s+inet6\s+(([0-9a-fA-F]{0,4}:){0,7}[0-9a-fA-F]{0,4})/[0-9]{1,3}\s+')
-def get_ipaddr(dev):
-        p=subprocess.Popen([bin_ip, 'address', 'show', 'dev', dev],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        r=p.communicate()
+def measure_ipv4(ipv4addr):
+    try:
+        ipo = ipaddress.IPv4Address(ipv4addr)
+    except:
+        return 0
 
-        ipv4 = []
-        ipv6 = []
+    if ipo.is_multicast:
+        return 0
+    if ipo.is_private:
+        return 0
+    if ipo.is_unspecified:
+        return 0
+    if ipo.is_reserved:
+        return 0
+    if ipo.is_loopback:
+        return 0
+    if ipo.is_link_local:
+        return 0
 
-        res = r[0].decode('ascii')
-        if res:
-                for l in res.split('\n'):
-                        m = ipv4regexp.match(l)
-                        if m:
-                                ipv4.append(m.group(1))
-
-                        m = ipv6regexp.match(l)
-                        if m:
-                                ipv6.append(m.group(1))
-        return (ipv4,ipv6)
-
-
-def select_ipv4(ipv4list):
-        if ipv4list:
-                for ip in ipv4list:
-                        ipo = ipaddress.IPv4Address(ip)
-                        if ipo.is_multicast:
-                                continue
-                        if ipo.is_private:
-                                continue
-                        if ipo.is_unspecified:
-                                continue
-                        if ipo.is_reserved:
-                                continue
-                        if ipo.is_loopback:
-                                continue
-                        if ipo.is_link_local:
-                                continue
-
-                        return ip
-
-def select_ipv6(ipv6list):        
-        if ipv6list:
-                noneui64=None
-                for ip in ipv6list:
-                        ipo = ipaddress.IPv6Address(ip)
-                        if ipo.is_multicast:
-                                continue
-                        if ipo.is_private:
-                                continue
-                        if ipo.is_unspecified:
-                                continue
-                        if ipo.is_reserved:
-                                continue
-                        if ipo.is_loopback:
-                                continue
-                        if ipo.is_link_local:
-                                continue
-                        if ipo.is_site_local:
-                                continue
-
-                        if ipo.packed[11] == 0xff and ipo.packed[12] == 0xfe:
-                                return ip
-
-                        if not noneui64:
-                                noneui64 = ip
-
-                return noneui64
+    return 1
 
 
-def get_hostaddr():
-        ipv4list = []
-        ipv6list = []
+def measure_ipv6(ipv6addr):
+    def is_eui64(ipa):
+        if ipa.packed[11] == 0xff and ipa.packed[12] == 0xfe:
+            return True
+        else:
+            return False
 
-        for i in interfaces:
-                r = get_ipaddr(i)
-                d("%s: %s" % (str(i), str(r)))
-                ipv4list += r[0]
-                ipv6list += r[1]
+    try:
+        ipo = ipaddress.IPv6Address(ip)
+    except:
+        return 0
 
-        ipv4 = select_ipv4(ipv4list)
-        ipv6 = select_ipv6(ipv6list)
+    if ipo.is_multicast:
+        return 0
+    if ipo.is_private:
+        return 0
+    if ipo.is_unspecified:
+        return 0
+    if ipo.is_reserved:
+        return 0
+    if ipo.is_loopback:
+        return 0
+    if ipo.is_link_local:
+        return 0
+    if ipo.is_site_local:
+        return 0
+    if is_eui64(ipo):
+        return 2
+
+    return 2
+
+
+def get_host_ipaddr(devs, enable_ipv4, enable_ipv6):
+    """
+    devs - None or list of str or str; str = name of interface(s)
+    enable_ipv4 - boot = include IPv4 addresses in the result
+    enable_ipv6 - bool = include IPv6 addresses in the result
+    return ([str IPv4],[str IPv6]), str = IP addresses
+    """
+
+    ipv4list = []
+    ipv6list = []
+
+    for addr,proto in get_dev_ipaddr(devs):
+        if proto == 4:
+            ipv4list.append(addr)
+        elif proto == 6:
+            ipv6list.append(addr)
+        else:
+            raise Exception("Wrong protocol: %d" % proto)
+
+    ipv4 = sorted(ipv4list, key=measure_ipv4, reverse=True)
+    ipv6 = sorted(ipv6list, key=measure_ipv6, reverse=True)
+
+    return (ipv4[0] if enable_ipv4 and ipv4 else None, ipv6[0] if enable_ipv6 and ipv6 else None)
+
+
+def normalize_dns(name):
+    return (name if name[-1] == '.' else "%s." % name)
+
+def denormalize_dns(name):
+    return (name[:-1] if name[-1] == '.' else name)
+
+
+def query_dns():
+    ipv4regexp = re.compile('([0-9]{1,3}\.){3}[0-9]{1,3}')
+    ipv6regexp = re.compile('([0-9a-fA-F]{0,4}:){1,7}[0-9a-fA-F]{0,4}')
+    def q(name,qtype):
+        regexpc=None
+        addrregexp=None
+        if qtype == 'A':
+            regexp = '^%s\s+[0-9]+\s+IN\s+A\s+([0-9\.]+)$' % name.replace('.', '\.')
+            regexpc = re.compile(regexp)
+            addrregexp = ipv4regexp
+        elif qtype == 'AAAA':
+            regexp = '^%s\s+[0-9]+\s+IN\s+AAAA\s+([0-9a-fA-F:]+)$' % name.replace('.', '\.')
+            regexpc = re.compile(regexp)
+            addrregexp = ipv6regexp
+        else:
+            raise Exception("Unknown qtype: %s" % str(qtype))
                 
-        return (ipv4 if enable_ipv4 else None, ipv6 if enable_ipv6 else None)
+        c=[config.bin_dig, '@%s' % config.dns_server, name, qtype]
+        dig=subprocess.Popen(c,stdout=subprocess.PIPE)
+        d("Running command: %s" % str(c))
+        r=dig.communicate()
+        d("Command finished. Returncode: %d. Output: %s" % (dig.returncode, str(r)))
+
+        if r and r[0]:
+            for l in r[0].decode('ascii').split('\n'):
+                m = regexpc.match(l)
+                if m and addrregexp.match(m.group(1)):
+                    return m.group(1)
+ 
+    dnsname = normalize_dns("%s.%s" % (config.hostname, config.dns_zone))
+    return(q(dnsname,'A'), q(dnsname,'AAAA'))
 
 
+def update_dns(ipv4=None, ipv6=None):
+        dnsname = normalize_dns("%s.%s" % (config.hostname, config.dns_zone))
+        commands = """server %s
+zone %s
+update del %s
+""" % (config.dns_server, denormalize_dns(config.dns_zone), dnsname)
 
-def report_data_rest(url,data,name=None,password=None):
-        h = httplib2.Http(disable_ssl_certificate_validation=True)
-        if name and password:
-                h.add_credentials(name, password)
-        resp, content = h.request(url, "PUT",
-                                  body=json.JSONEncoder().encode(data),
-                                  headers={'content-type':'application/json'} )
+        if ipv6:
+                commands += "update add %s %d AAAA %s\n" % (dnsname, config.rr_ttl, ipv6)
+        if ipv4:
+                commands += "update add %s %d A %s\n" % (dnsname, config.rr_ttl, ipv4)
 
-        if int(resp['status']) == 200:
-                d("Reported %s OK." % str(data))
-                d("Result: %s" % str(content))
-        else:
-                log("Reporting %s failed with code %s." % (str(data), str(resp['status'])))
-                d("Result: %s" % str(content))
+        commands += "send\n"
+
+        d("Running command %s -y <hidden>" % config.bin_nsupdate)
+        nsu=subprocess.Popen([config.bin_nsupdate, "-y", config.nsupdate_key],
+                             stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        d("Feeding data: \n%s---------------" % commands)
+        r=nsu.communicate(commands.encode("ascii"))
+        d("Update finished. Return code: %d. Output: %s" % (nsu.returncode, str(r)))
 
 
 def main():
@@ -155,39 +225,63 @@ def main():
                 print("""reportip.py by Tomas Hlavacek (tmshlvck@gmail.com)
   -d --debug : sets debugging output
   -h --help : prints this help message
+  -q --query : just run DNS query and print status, do not update
+  -f --force : force update
 """)
 
         try:
-                opts, args = getopt.getopt(sys.argv[1:], "hd", ["help", "debug"])
+                opts, args = getopt.getopt(sys.argv[1:], "hdqf", ["help", "debug", "query", "force"])
         except getopt.GetoptError as err:
                 print(str(err))
                 usage()
                 sys.exit(2)
 
+        force_update = False
         for o, a in opts:
                 if o == '-d':
                         debug = 1
                 elif o == '-h':
                         usage()
                         sys.exit(0)
+                elif o == '-q':
+                    print("Hostname: %s, zone: %s, DNS server: %s" % (config.hostname, config.dns_zone, config.dns_server))
+                    (ipv4,ipv6) = get_host_ipaddr(config.interfaces, config.enable_ipv4, config.enable_ipv6)
+                    print("Local addresses: IPv4 %s, IPv6 %s" % (str(ipv4), str(ipv6)))
+                    (dns_ipv4,dns_ipv6) = query_dns()
+                    print("Addresses in DNS: IPv4 %s, IPv6 %s" % (str(dns_ipv4), str(dns_ipv6)))
+                    return
+
+                elif o == '-f':
+                    force_update = True
+
                 else:
-                        assert False, "Unhandled option"
+                    assert False, "Unhandled option"
 
 
-        if sleep:
-                d("Sleeping for %d s." % sleep)
-                time.sleep(sleep)
+        if config.sleep:
+                d("Sleeping for %d s." % config.sleep)
+                time.sleep(config.sleep)
+                d("Waking up...")
 
         # report addresses
-        (ipv4,ipv6) = get_hostaddr()
+        (ipv4,ipv6) = get_host_ipaddr(config.interfaces, config.enable_ipv4, config.enable_ipv6)
+        d("Local addresses: IPv4 %s, IPv6 %s" % (str(ipv4), str(ipv6)))
         if ipv4 == None and ipv6 == None:
-            # no connectivity -> nothing to report
-            d("No connectivity, nothing to report.")
+            d("No connectivity, nothing to report. Noop. Finish.")
             return
-        data = {'ipv4':ipv4, 'ipv6':ipv6}
-        d("Sending: %s" % str(data))
-        report_data_rest(report_url, data, http_name, http_password)
-                
+
+        (dns_ipv4,dns_ipv6) = query_dns()
+        d("Addresses in DNS: IPv4 %s, IPv6 %s" % (str(dns_ipv4), str(dns_ipv6)))
+
+        if ipv4 != dns_ipv4 or ipv6 != dns_ipv6 or force_update:
+            d("Difference in local address and DNS. Sending update.")
+            update_dns(ipv4, ipv6)
+        else:
+            d("No difference in local address and DNS. Noop.")
+
+        d("Finish.")
+
+         
 
 if __name__ == '__main__':
         main()
--- client/reportipconf.py
+++ client/reportipconf.py
@@ -2,13 +2,20 @@
 # Change the values to proper paths/ifaces/URLs
 #
 # Config for DDNS reportip.py
-report_url = "https://<host>/ddns/rest/update/<name>"
-http_name = '<name>'
-http_password = '<passwd>'
+
+hostname="testhost"
 enable_ipv6=True
 enable_ipv4=False
 interfaces=['eth0','wlan0']
 bin_ip='/bin/ip'
-sleep = 0 # add some time when using the script with network manager
+sleep = 0 # add some time when using the script with NetworkManager
 debug = 1
+
+bin_nsupdate = '/usr/bin/nsupdate'
+bin_dig = '/usr/bin/dig'
+nsupdate_key = 'hmac-md5:<keyname>:<secret>'
+
+dns_server = 'mydnsserver.org'
+dns_zone = 'zone.tld'
+rr_ttl = 60
 
